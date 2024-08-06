46f55c2ad3a565a6ca20b0ef15a438a4203dc5ef
停P <40131396+stop-pattern@users.noreply.github.com>
2024-08-07 00:51:12 +0900

    add main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
+#include <driver/ledc.h>
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr uint8_t pin = 27;
+
+// 文字列がintに変換可能か
+bool isInt(String str) {
+    for (auto &&i : str) {
+        // if (!isdigit(i)) return false;
+        Serial.print(isdigit(i));
+    }
+    return true;
+}
+
+// ledcでPFM
+void pfmWrite(uint32_t freq) {
+    ledcSetup(LEDC_CHANNEL_0, freq, LEDC_TIMER_12_BIT);
+    ledcWrite(LEDC_CHANNEL_0, 2048);// 50%
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Serial.println("setup");
+    
+    pinMode(pin, OUTPUT);
+    
+    ledcAttachPin(pin, LEDC_CHANNEL_0);
+    pfmWrite(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if(Serial.available()){
+        String str = Serial.readStringUntil('\n');
+        uint32_t freq = str.toInt();
+        if (freq == 0) {
+            digitalWrite(pin, LOW);
+        }
+        else{
+            pfmWrite(freq);
+        }
+        Serial.print("set freq: ");
+        Serial.println(freq);
+    }
+    delay(5);
 }
